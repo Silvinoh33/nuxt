96fa43d78041ce9f80e4975342c3dd66276d35db
Silvinoh <edhorgounon@gmail.com>
2024-05-17 12:26:13 +0200

    Ajout de tailwind et du modu Icon

--- app.vue
+++ app.vue
@@ -1,5 +1,7 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="text-4xl text-indigo-500 font-thin">
+    Hello world ! Nuxt 
+
+    <Icon name="devicon:githubcodespaces"/>
   </div>
 </template>
