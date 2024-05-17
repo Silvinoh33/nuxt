02ad76b01f78a9e12f1ea6011ab965b0619a2d78
Silvinoh <edhorgounon@gmail.com>
2024-05-17 12:35:21 +0200

    first Component : Hello.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,3 @@
 <template>
-  <div class="text-4xl text-indigo-500 font-thin">
-    Hello world ! Nuxt 
-
-    <Icon name="devicon:githubcodespaces"/>
-  </div>
+  <Hello />
 </template>
---
+++ components/Hello.vue
@@ -0,0 +1,9 @@
+<template >
+   <div >
+    <h2 class="text-4xl text-emerald-500 font-thin">Hello world ! Nuxt </h2>
+<p> Avez vous un compte Github
+  <Icon name="devicon:githubcodespaces"/>
+
+</p>
+  </div>
+</template>
